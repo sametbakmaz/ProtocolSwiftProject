1dba9f2d45f9e4fe9ad08c8cfec0b40212520033
sametbakmaz <sametbkmz@gmail.com>
2022-10-03 11:55:21 +0300

    çalışmalar eklendi

--- ProtocolSwiftProject/main.swift
+++ ProtocolSwiftProject/main.swift
@@ -7,5 +7,43 @@
 
 import Foundation
 
-print("Hello, World!")
+protocol Running {
+    func myRun()
+}
+class Animal{
+    func running(){
+        print("running")
+    }
+    func test () {
+        print("test")
+    }
+}
+class Dog : Animal, Running { //protocolü inherit edeceksek onun içindeki fonksiyonları da kullanmak zorundayız. Aslında bunu bir kütüphane çağırdğımızda yani delegate ettiğimizde, örneğin tableView için onun gerekli fonksiyonlarını bizden istediğini görmüştük. Aynı hatayı daha öncsinde de almıştık. Burada kendi planımızı oluşturduk
+    func myRun() {
+        print("running")
+    }
+}
+let karabas = Dog()
+karabas.myRun()
+karabas.test()
+
+class Cat : Animal {
+    
+}
+let minnos = Cat()
+minnos.running()
+
+class Turttle : Animal {
+    
+}
+let leonardo = Turttle()
+leonardo.running()
+
+struct Bird : Running { //structa class inherit edemedim fakat bir protocolü inherit edebildim
+    func myRun() {
+        print("running")
+    }
+}
+let tweety = Bird()
+tweety.myRun()
 
